9d3c2a873c02940a92bcbdd6b48f314cf8f04890
christopheradriankusuma <christopheradrian2903@gmail.com>
2021-11-08 21:06:51 +0700

    commit

--- Water7/script.sh
+++ Water7/script.sh
@@ -1,4 +1,11 @@
-echo "nameserver 192.168.122.1" > /etc/resolv.conf
+echo "nameserver 192.215.2.2" > /etc/resolv.conf
+echo "nameserver 192.168.122.1" >> /etc/resolv.conf
 
 apt update
-apt install squid zip unzip -y
+apt install squid zip unzip apache2-utils -y
+
+htpasswd -bc /etc/squid/htpasswd luffybelikapalt08 luffy_t08
+htpasswd -b /etc/squid/htpasswd zorobelikapalt08 zoro_t08
+
+unzip -o -d /etc squid.zip
+service squid restart
